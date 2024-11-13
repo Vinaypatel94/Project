bf2ba51a21789a76261c904cb7ef51af403bbb45
Vinaypatel94 <vinaypate94@gmail.com>
2024-11-13 16:02:34 +0530

    add initial folder

---
+++ Continuous-change-BGColor/script.js
@@ -0,0 +1,28 @@
+// 
+
+const randomColor=function(){
+    const hex="0123456789ABCDEF";
+    let color='#';
+    for(let i=0;i<6;i++){
+        color+=hex[Math.floor(Math.random()*16)];
+    }
+    return color; 
+};
+// console.log(randomColor());
+let intervalId;
+const startChangingColor=function(){
+    if(!intervalId){
+        intervalId=setInterval(changeBgcolor,1000);
+    }
+    function changeBgcolor(){
+        document.body.style.backgroundColor=randomColor();
+        console.log(randomColor)
+    }
+};
+const stopChangingColor=function(){
+    clearInterval(intervalId);
+    intervalId=null;
+};
+document.querySelector(".start").addEventListener("click",startChangingColor);
+document.querySelector('.stop').addEventListener("click",stopChangingColor);
+
