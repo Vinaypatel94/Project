a909cd909be455a9c33d36a5381f09160070475e
Vinaypatel94 <vinaypate94@gmail.com>
2024-11-16 23:28:18 +0530

    add folder

---
+++ BG-Color_changer/script.js
@@ -0,0 +1,16 @@
+const aqua=document.querySelector('.btn1');
+aqua.addEventListener('click',function(){
+   document.querySelector('body').style.backgroundColor="aqua";
+})
+const brown=document.querySelector('.btn2');
+brown.addEventListener('click',function(){
+    document.querySelector('body').style.backgroundColor="brown";
+ })
+const blueviolet=document.querySelector('.btn3');
+blueviolet.addEventListener('click',function(){
+    document.querySelector('body').style.backgroundColor="blueviolet";
+ })
+const yellow=document.querySelector('.btn4');
+yellow.addEventListener('click',function(){
+    document.querySelector('body').style.backgroundColor="yellow";
+ })
